2bf44eba3b83388249c8a6e75e1cb96eda23425f
loki <ya@fagundes.ru>
2024-12-18 16:13:12 +0300

    work with users

--- app/core/config.py
+++ app/core/config.py
@@ -10,6 +10,7 @@ class Settings(BaseSettings):
     app_title: str = "Бронирование переговорок"
     description: str = "Здесь можно всё!"
     database_url: str
+    secret: str = "SECRET"
 
     class Config:
         env_file = ".env"
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,3 @@
-from app.models.meeting_room import MeetingRoom
-from app.models.reservation import Reservation
+from .meeting_room import MeetingRoom
+from .reservation import Reservation
+from .user import User
